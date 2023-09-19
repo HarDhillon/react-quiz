5e982eecfeb796940cd6236baee41c485693a3ef
user-personal <har7173@gmail.com>
2023-09-19 15:54:58 +0100

    remove no longer required custom hook

--- src/hooks/getOptions.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const getOptions = async () => {
-    const response = await axios.get("https://opentdb.com/api_config.php")
-
-    return response
-}
-
-export default getOptions
