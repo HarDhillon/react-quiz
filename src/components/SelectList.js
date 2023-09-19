13d6bb60927958e65755838c5bba7a33c4d8a236
user-personal <har7173@gmail.com>
2023-09-19 16:32:57 +0100

    create select lists

--- src/components/SelectList.js
+++ src/components/SelectList.js
@@ -1,5 +1,10 @@
-function SelectList() {
-    return <div>Select List</div>
+function SelectList({options}) {
+
+    const renderedOptions = options.map((option) => {
+        return <option key={option.value} value={option.value}>{ option.label }</option>
+    })
+
+    return <select>{ renderedOptions }</select>
 }
 
 export default SelectList
--- src/pages/QuizPage.js
+++ src/pages/QuizPage.js
@@ -2,19 +2,56 @@ import { useEffect } from "react"
 import { useFetchQuestionsQuery } from "../store";
 import SelectList from "../components/SelectList";
 import Question from "../components/Question"
-import getOptions from "../hooks/getOptions";
 
 function QuizPage() {
     
     // const { data, error, isFetching } = useFetchQuestionsQuery(options)
 
-    useEffect(() => {
-        getOptions()
-    }, [])
+    const questionsMax = 50
+
+    const questionsCategory = [
+        { value: 'any', label: 'Any Category' },
+        { value: '9', label: 'General Knowledge' },
+        { value: '10', label: 'Entertainment: Books' },
+        { value: '11', label: 'Entertainment: Film' },
+        { value: '12', label: 'Entertainment: Music' },
+        { value: '13', label: 'Entertainment: Musicals &amp; Theatres' },
+        { value: '14', label: 'Entertainment: Television' },
+        { value: '15', label: 'Entertainment: Video Games' },
+        { value: '16', label: 'Entertainment: Board Games' },
+        { value: '17', label: 'Science &amp; Nature' },
+        { value: '18', label: 'Science: Computers' },
+        { value: '19', label: 'Science: Mathematics' },
+        { value: '20', label: 'Mythology' },
+        { value: '21', label: 'Sports' },
+        { value: '22', label: 'Geography' },
+        { value: '23', label: 'History' },
+        { value: '24', label: 'Politics' },
+        { value: '25', label: 'Art' },
+        { value: '26', label: 'Celebrities' },
+        { value: '27', label: 'Animals' },
+        { value: '28', label: 'Vehicles' },
+        { value: '29', label: 'Entertainment: Comics' },
+        { value: '30', label: 'Science: Gadgets' },
+        { value: '31', label: 'Entertainment: Japanese Anime &amp; Manga' },
+        { value: '32', label: 'Entertainment: Cartoon &amp; Animations' }
+      ]
+
+    const questionsDifficulty = [
+        { value: "any", label: "Any" }, { value: "easy", label: "Easy" }, { value: "medium", label: "Medium" }, { value: "hard", label: "Hard" }
+    ]
+
+    const questionsType = [
+        {value: "any" , label: "Any" }, {value: "multiple" , label: "Multiple Choice" }, {value: "boolean" , label: "True / False" }
+    ]
+
+
 
     return (
         <div>
-            <SelectList options={options}></SelectList>
+            <SelectList options={questionsCategory} ></SelectList>
+            <SelectList options={questionsDifficulty}></SelectList>
+            <SelectList options={questionsType}></SelectList>
         </div>
     )
 }
