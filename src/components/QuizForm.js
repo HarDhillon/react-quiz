246f49754a14ff2399a29a85961ad15bd24b3706
HarDhillon <har7173@gmail.com>
2023-11-25 15:15:26 +0000

    change form toggler from jsx to using class to hide

--- src/components/QuizForm.js
+++ src/components/QuizForm.js
@@ -121,37 +121,35 @@ function QuizForm() {
 
 
     return (
-        <Panel className={"p-5 relative"}>
-            {isExpanded && (
-                <form onSubmit={handleSubmit}>
-                    <div className="flex flex-wrap justify-between mb-4">
-                        <div className="form-field">
-                            <label>Number of Questions (1 - 50)</label>
-                            <input className="p-1 border-2 border-gray-500 focus:outline-none focus:border-black" value={amount || 1} onChange={handleNumberChange}></input>
-                        </div>
-
-                        <div className="form-field">
-                            <label>Category</label>
-                            <select className="p-3">{categoryItems}</select>
-                        </div>
-
-                        <div className="form-field">
-                            <label>Difficulty</label>
-                            <select className="p-3">{difficultyItems}</select>
-                        </div>
-
-                        <div className="form-field">
-                            <label>Type</label>
-                            <select className="p-3">{typeItems}</select>
-                        </div>
-
+        <Panel className={`p-5 relative`}>
+            <form className={`quiz-form ${isExpanded ? 'open' : 'closed'}`} onSubmit={handleSubmit}>
+                <div className="flex flex-wrap justify-between mb-4">
+                    <div className="form-field">
+                        <label>Number of Questions (1 - 50)</label>
+                        <input className="p-1 border-2 border-gray-500 focus:outline-none focus:border-black" value={amount || 1} onChange={handleNumberChange}></input>
                     </div>
 
-                    <Button className={"hover:bg-gray-200 transition-colors"}>Generate Questions</Button>
-                </form>
+                    <div className="form-field">
+                        <label>Category</label>
+                        <select className="p-3">{categoryItems}</select>
+                    </div>
 
-            )}
-            <div onClick={handleClick} className="bg-gray-100 hover:bg-gray-300 hover:cursor-pointer absolute bottom-0 p-1 rounded-full border border-black left-1/2 -translate-x-1/2 translate-y-1/2">{icon}</div>
+                    <div className="form-field">
+                        <label>Difficulty</label>
+                        <select className="p-3">{difficultyItems}</select>
+                    </div>
+
+                    <div className="form-field">
+                        <label>Type</label>
+                        <select className="p-3">{typeItems}</select>
+                    </div>
+
+                </div>
+
+                <Button className={"hover:bg-gray-200 transition-colors"}>Generate Questions</Button>
+            </form>
+
+            <div onClick={handleClick} className="toggler bg-gray-100 hover:bg-gray-300 hover:cursor-pointer absolute bottom-0 p-1 rounded-full border border-black left-1/2 -translate-x-1/2 translate-y-1/2">{icon}</div>
         </Panel>
 
     )
