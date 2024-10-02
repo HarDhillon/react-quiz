207c3a4639ba6e3dfa9f9090e5e80c23f20b031a
HarDhillon <har7173@gmail.com>
2024-10-02 21:26:02 +0100

    refactor(QuizForm) - Made dispatch functions and options generation more reuseable - Moved options configuration into different file to reduce file length

--- src/components/QuizForm.js
+++ src/components/QuizForm.js
@@ -1,117 +1,53 @@
-import { useDispatch, useSelector } from "react-redux"
-import { changeAmount, changeCategory, changeDifficulty, changeQuestionsAnswered, changeSubmitted, changeType, changeUserScore } from '../store'
-import Button from "./Button"
-import Panel from "./Panel"
-import { GoArrowDown, GoArrowUp } from "react-icons/go"
-import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { changeAmount, changeCategory, changeDifficulty, changeQuestionsAnswered, changeSubmitted, changeType, changeUserScore } from "../store";
+import { optionsCategory, optionsDifficulty, optionsType } from '../config/quizOptions'
+import Button from "./Button";
+import Panel from "./Panel";
+import { GoArrowDown, GoArrowUp } from "react-icons/go";
+import { useState } from "react";
 
 
 function QuizForm() {
+    const dispatch = useDispatch();
+    const amount = useSelector((state) => state.config.amount);
 
-    const optionsCategory = [
-        { value: 'any', label: 'Any Category' },
-        { value: '9', label: 'General Knowledge' },
-        { value: '10', label: 'Entertainment: Books' },
-        { value: '11', label: 'Entertainment: Film' },
-        { value: '12', label: 'Entertainment: Music' },
-        { value: '13', label: 'Entertainment: Musicals &amp; Theatres' },
-        { value: '14', label: 'Entertainment: Television' },
-        { value: '15', label: 'Entertainment: Video Games' },
-        { value: '16', label: 'Entertainment: Board Games' },
-        { value: '17', label: 'Science &amp; Nature' },
-        { value: '18', label: 'Science: Computers' },
-        { value: '19', label: 'Science: Mathematics' },
-        { value: '20', label: 'Mythology' },
-        { value: '21', label: 'Sports' },
-        { value: '22', label: 'Geography' },
-        { value: '23', label: 'History' },
-        { value: '24', label: 'Politics' },
-        { value: '25', label: 'Art' },
-        { value: '26', label: 'Celebrities' },
-        { value: '27', label: 'Animals' },
-        { value: '28', label: 'Vehicles' },
-        { value: '29', label: 'Entertainment: Comics' },
-        { value: '30', label: 'Science: Gadgets' },
-        { value: '31', label: 'Entertainment: Japanese Anime &amp; Manga' },
-        { value: '32', label: 'Entertainment: Cartoon &amp; Animations' }
-    ]
+    const [isExpanded, setIsExpanded] = useState(true);
 
-
-    const optionsDifficulty = [
-        { value: "any", label: "Any" }, { value: "easy", label: "Easy" }, { value: "medium", label: "Medium" }, { value: "hard", label: "Hard" }
-    ]
-
-
-    const optionsType = [
-        { value: "any", label: "Any" }, { value: "multiple", label: "Multiple Choice" }, { value: "boolean", label: "True / False" }
-    ]
-
-    const [isExpanded, setIsExpanded] = useState(true)
-
-    const handleClick = () => {
-        setIsExpanded(!isExpanded)
-    }
-
-    const dispatch = useDispatch()
-
-    const amount = useSelector((state) => {
-        return state.config.amount
-    })
+    const handleClick = () => setIsExpanded(!isExpanded);
 
     const resetQuiz = () => {
-        dispatch(changeSubmitted(false))
-        dispatch(changeQuestionsAnswered(0))
-        dispatch(changeUserScore(0))
-    }
+        dispatch(changeSubmitted(false));
+        dispatch(changeQuestionsAnswered(0));
+        dispatch(changeUserScore(0));
+    };
+
+    const handleInputChange = (actionCreator) => (e) => {
+        dispatch(actionCreator(e.target.value));
+        resetQuiz();
+    };
 
     const handleNumberChange = (e) => {
-        // const re = /^[0-9\b]+$/;
-        const number = parseInt(e.target.value)
-
+        const number = parseInt(e.target.value);
         if (isNaN(number)) {
-            dispatch(changeAmount(1))
-            return
+            dispatch(changeAmount(1));
+        } else if (number > 0 && number <= 50) {
+            dispatch(changeAmount(number));
         }
-        if (number > 50 || number <= 0) {
-            return
-        }
-
-        dispatch(changeAmount(number))
-        resetQuiz()
-    }
-    const handleCategoryClick = (e) => {
-        dispatch(changeCategory(e.target.value))
-        resetQuiz()
-    }
-
-    const handleDifficultyClick = (e) => {
-        dispatch(changeDifficulty(e.target.value))
-        resetQuiz()
-    }
-
-    const handleTypeclick = (e) => {
-        dispatch(changeType(e.target.value))
-        resetQuiz()
-    }
+        resetQuiz();
+    };
 
     const handleSubmit = (e) => {
-        e.preventDefault()
-        dispatch(changeSubmitted(true))
-        setIsExpanded(false)
-    }
+        e.preventDefault();
+        dispatch(changeSubmitted(true));
+        setIsExpanded(false);
+    };
 
-
-    const categoryItems = optionsCategory.map((item) => {
-        return <option onClick={handleCategoryClick} key={item.value} value={item.value}>{item.label}</option>
-    })
-
-    const difficultyItems = optionsDifficulty.map((item) => {
-        return <option onClick={handleDifficultyClick} key={item.value} value={item.value}>{item.label}</option>
-    })
-
-    const typeItems = optionsType.map((item) => {
-        return <option onClick={handleTypeclick} key={item.value} value={item.value}>{item.label}</option>
-    })
+    const renderOptions = (options) =>
+        options.map(({ value, label }) => (
+            <option key={value} value={value}>
+                {label}
+            </option>
+        ));
 
     const icon = (
         <span className="text-2xl">
@@ -119,40 +55,52 @@ function QuizForm() {
         </span>
     );
 
-
     return (
-        <Panel className={`p-5 relative`}>
-            <form className={`quiz-form ${isExpanded ? 'open' : 'closed'}`} onSubmit={handleSubmit}>
+        <Panel className="p-5 relative">
+            <form className={`quiz-form ${isExpanded ? "open" : "closed"}`} onSubmit={handleSubmit}>
                 <div className="flex flex-wrap justify-between mb-4">
                     <div className="form-field">
                         <label>Number of Questions (1 - 50)</label>
-                        <input className="p-1 border-2 border-gray-500 focus:outline-none focus:border-black" value={amount || 1} onChange={handleNumberChange}></input>
+                        <input
+                            className="p-1 border-2 border-gray-500 focus:outline-none focus:border-black"
+                            value={amount || 1}
+                            onChange={handleNumberChange}
+                        />
                     </div>
 
                     <div className="form-field">
                         <label>Category</label>
-                        <select className="p-3">{categoryItems}</select>
+                        <select className="p-3" onChange={handleInputChange(changeCategory)}>
+                            {renderOptions(optionsCategory)}
+                        </select>
                     </div>
 
                     <div className="form-field">
                         <label>Difficulty</label>
-                        <select className="p-3">{difficultyItems}</select>
+                        <select className="p-3" onChange={handleInputChange(changeDifficulty)}>
+                            {renderOptions(optionsDifficulty)}
+                        </select>
                     </div>
 
                     <div className="form-field">
                         <label>Type</label>
-                        <select className="p-3">{typeItems}</select>
+                        <select className="p-3" onChange={handleInputChange(changeType)}>
+                            {renderOptions(optionsType)}
+                        </select>
                     </div>
-
                 </div>
 
-                <Button className={"hover:bg-gray-200 transition-colors"}>Generate Questions</Button>
+                <Button className="hover:bg-gray-200 transition-colors">Generate Questions</Button>
             </form>
 
-            <div onClick={handleClick} className="toggler bg-gray-100 hover:bg-gray-300 hover:cursor-pointer absolute bottom-0 p-1 rounded-full border border-black left-1/2 -translate-x-1/2 translate-y-1/2">{icon}</div>
+            <div
+                onClick={handleClick}
+                className="toggler bg-gray-100 hover:bg-gray-300 hover:cursor-pointer absolute bottom-0 p-1 rounded-full border border-black left-1/2 -translate-x-1/2 translate-y-1/2"
+            >
+                {icon}
+            </div>
         </Panel>
-
-    )
+    );
 }
 
-export default QuizForm
+export default QuizForm;
