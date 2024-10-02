6476abbc554cc84b07529c1f808f9a5ea45a781b
HarDhillon <har7173@gmail.com>
2024-10-02 22:05:44 +0100

    style(card): provides more visual feedback when right or wrong

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,6 +3,8 @@ import classNames from 'classnames';
 function Card({ children, className, userCorrect, selectedChoice, ...rest }) {
     const finalClassNames = classNames(
         'border rounded p-6 text-center shadow-2xl',
+        userCorrect && selectedChoice ? 'bg-green-200' : '',
+        !userCorrect && selectedChoice ? 'bg-red-200' : '',
         className
     );
 
